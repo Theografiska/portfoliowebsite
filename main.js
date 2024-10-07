21ea09db534febc09e97499167e27bf9076d6377
Theodor Kaljo <theodor.kaljo@gmail.com>
2024-10-07 22:35:58 +0300

    added most of the hero section and some js for the typewriter effect

---
+++ main.js
@@ -0,0 +1,42 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const typewriterText = document.querySelector(".typewriter-text");
+    const texts = [
+        "Front End Developer",
+        "Back End Developer (soon)",
+        "Full Stack Developer (soon)"
+    ];
+
+    let index = 0;
+    let charIndex = 0;
+    let currentText = "";
+    let isDeleting = false;
+
+    function type() {
+        if (index < texts.length) {
+            if (!isDeleting && charIndex < texts[index].length) {
+                // Typing text
+                currentText += texts[index].charAt(charIndex);
+                charIndex++;
+                typewriterText.textContent = currentText;
+                setTimeout(type, 100);
+            } else if (isDeleting && charIndex >= 0) {
+                // Deleting text
+                currentText = currentText.substring(0, charIndex);
+                charIndex--;
+                typewriterText.textContent = currentText;
+                setTimeout(type, 50);
+            } else {
+                // Switch between typing and deleting
+                isDeleting = !isDeleting;
+
+                if (!isDeleting) {
+                    index++;
+                    if (index === texts.length) index = 0;
+                }
+                setTimeout(type, 500);
+            }
+        }
+    }
+
+    type();
+});
